062db4a33106dde2902340abda1f139fd9983c2a
QWitcherS <quickwitcher@gmail.com>
2023-07-26 14:25:40 +0500

    Реализация игрового процесса №1

--- components/level-pages.js
+++ components/level-pages.js
@@ -1,65 +1,66 @@
-// import { initRenderLevelGame } from './page-level.js'
+import { initRenderLevelGame } from './page-level.js'
 
-// export function renderRadioComponents() {
-//     const appHtml = `
-//   <div class="conteiner center">
-//   <form class="content">
-//     <h1 class="heading" id="headingLevel">Выбери сложность</h1>
-//     <div class="radio-toolbar">
-//       <input type="radio" id="radio1" name="difficulty" value="easy" />
-//       <label for="radio1">1</label>
+export function renderRadioComponents() {
+    const app = document.getElementById('app')
+    const appHtml = `
+  <div class="conteiner center">
+  <form class="content">
+    <h1 class="heading" id="headingLevel">Выбери сложность</h1>
+    <div class="radio-toolbar">
+      <input type="radio" id="radio1" name="difficulty" value="easy" />
+      <label for="radio1">1</label>
 
-//       <input type="radio" id="radio2" name="difficulty" value="medium" />
-//       <label for="radio2">2</label>
+      <input type="radio" id="radio2" name="difficulty" value="medium" />
+      <label for="radio2">2</label>
 
-//       <input type="radio" id="radio3" name="difficulty" value="hard" />
-//       <label for="radio3">3</label>
-//     </div>
-//     <button type="submit" class="button" id="btnStart">Старт</button>
-//   </form>
-// </div>
-// `
+      <input type="radio" id="radio3" name="difficulty" value="hard" />
+      <label for="radio3">3</label>
+    </div>
+    <button type="submit" class="button" id="btnStart">Старт</button>
+  </form>
+</div>
+`
 
-//     app.innerHTML = appHtml
+    app.innerHTML = appHtml
 
-//     const levelData = {
-//         difficulty: 'medium',
-//         numCards: 12,
-//     }
+    const levelData = {
+        difficulty: 'medium',
+        numCards: 12,
+    }
 
-//     const difficultyInputs = document.querySelectorAll(
-//         '.radio-toolbar input[name="difficulty"]',
-//     )
+    const difficultyInputs = document.querySelectorAll(
+        '.radio-toolbar input[name="difficulty"]',
+    )
 
-//     const gameButton = document.querySelector('#game-button')
+    const gameButton = document.querySelector('#game-button')
 
-//     function levelNumCards() {
-//         switch (gameData.difficulty) {
-//             case 'easy':
-//                 levelData.numCards = 6
-//                 break
-//             case 'medium':
-//                 levelData.numCards = 12
-//                 break
-//             case 'hard':
-//                 levelData.numCards = 18
-//                 break
-//         }
-//     }
+    function levelNumCards() {
+        switch (gameData.difficulty) {
+            case 'easy':
+                levelData.numCards = 6
+                break
+            case 'medium':
+                levelData.numCards = 12
+                break
+            case 'hard':
+                levelData.numCards = 18
+                break
+        }
+    }
 
-//     difficultyInputs.forEach((input) => {
-//         input.addEventListener('change', () => {
-//             levelData.difficulty = input.value
-//             levelNumCards()
-//         })
-//     })
+    difficultyInputs.forEach((input) => {
+        input.addEventListener('change', () => {
+            levelData.difficulty = input.value
+            levelNumCards()
+        })
+    })
 
-//     gameButton.addEventListener('click', () => {
-//         levelGame(levelData.difficulty)
-//     })
+    gameButton.addEventListener('click', () => {
+        levelGame(levelData.difficulty)
+    })
 
-//     function levelGame(difficulty) {
-//         console.log(`Запуск игры с уровнем сложности "${difficulty}"`)
-//         initRenderLevelGame(difficulty)
-//     }
-// }
+    function levelGame(difficulty) {
+        console.log(`Запуск игры с уровнем сложности "${difficulty}"`)
+        initRenderLevelGame(difficulty)
+    }
+}
